8c8aefbb333eb72e0894ecc2fe0c638b20aa08a5
Sidecoder#1818 <91298464+sidecod@users.noreply.github.com>
2023-10-09 19:30:42 -0700

    Update main.js

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,14 @@
 (function () {
   const win = window
   const doc = document.documentElement
-
+  
+  let isChrome = /Chrome/.test(navigator.userAgent) && /Google Inc/.test(navigator.vendor);
+  let isSafari = /Safari/.test(navigator.userAgent) && /Apple Computer/.test(navigator.vendor);
+  let scrollbarDiv = document.querySelector('.scrollbar');
+    if (!isChrome && !isSafari) {
+      scrollbarDiv.innerHTML = 'You need Webkit browser to run this code';
+    }
+  
   doc.classList.remove('no-js')
   doc.classList.add('js')
 
